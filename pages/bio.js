1e764d3b65b5c0cf92d7f3c5f9089b8e47bd05c4
Carlos Iván Suarez <me@carlossz.co>
2017-11-23 09:48:36 -0500

    Update_info_bio

--- pages/bio.js
+++ pages/bio.js
@@ -7,11 +7,11 @@ const Bio = () => (
 		<div className="logo"><img src="/static/cartoon-blue.png" alt="Logo"/></div>
 		<div className="txt">
 			<h1 className="txtTitle">Biografia</h1>
-			<p>Mi nombre es Carlos Iván Suárez pero mi usuario en redes sociales siempre ha sido Carlos Sz, soy diseñador web de Colombia, todo mi conocimiento ha sido desarrollado de forma empírica ya que no me he llevado bien con los sistemas de educación tradicionales.</p>
-			<p>Asistí durante seis semestres a la universidad a la carrera de Ingeniería Electromécanica pero no me sentía satisfecho con los conocimientos que me estaban impartiendo, por ese tiempo conocí la plataforma de mejorando.la posteriormente llamada Platzi y me enamoré de su plataforma y su comunidad y actualmente soy parte de ella.</p>
-			<p>Mis conocimientos están enfocados en el diseño de interfaz de usuario (UI) y experiencia de usuario (UX), también se codear en html, css y estoy aprendiendo Javascript.</p>
-			<p>Mi experiencia ha sido como técnico en sistemas al principio como Diseñador Web y áctualmente como líder de Frontend en la oficina de Bogotá de la multinacional DDB, el cargo que hasta el momento considero que es lo que deseo hacer toda mi vida y disfruto mucho. Soy parte del programa de expertos en productos de Google desde el año 2011 el cual me ha permitido conocer muchos países.</p>
-			<p>Me encanta viajar, la música, la tecnología, el diseño, la programación, los idiomas (hablo Inglés fluído y estoy aprendiendo Francés) y salir de mi zona de confort siempre que puedo.</p>
+			<p>Mi nombre es Carlos Iván Suárez pero en internet soy más conocido como Carlos Sz, soy Frontend Developer actualmente en Bogotá, Colombia, todo mi conocimiento ha sido desarrollado de forma empírica ya que no creo en los sistemas de educación tradicionales.</p>
+			<p>Asistí durante seis semestres a la universidad a la carrera de Ingeniería Electromécanica pero no me sentía satisfecho con los conocimientos que me estaban impartiendo, por ese tiempo conocí la plataforma de mejorando.la posteriormente llamada Platzi, me enamoré de su plataforma y su comunidad y actualmente soy parte de ella. Junto con la plataforma de Treehouse son mis dos sitios preferidos para estudiar. </p>
+			<p>Mis conocimientos están enfocados en el diseño de interfaz de usuario (UI) y experiencia de usuario (UX), también el desarrollo de sitios y aplicaciones web en Html, Css, Javascript, actualmente enfocandome en ReactJS.</p>
+			<p>Mi experiencia ha sido como técnico en sistemas al principio, luego como Diseñador Web y áctualmente como líder de Frontend en la oficina de Bogotá de la multinacional DDB, el cargo que hasta el momento considero que es lo que deseo hacer toda mi vida y disfruto mucho. Soy parte del programa de expertos en productos de Google desde el año 2011 el cual me ha permitido conocer muchos países y vivir la cultura Googler a la cual espero pertenecer en poco tiempo.</p>
+			<p>Me encanta viajar, la música, la tecnología, el diseño, la programación, los idiomas (hablo Inglés fluído y estoy aprendiendo Francés) y salir de mi zona de confort siempre que puedo ya que me parece la mejor forma de lograr grandes cosas.</p>
 			<Button msg="Descarga mi CV" target href="//drive.google.com/file/d/0B_gktS92l6CCb1lfaGgyWTBOeDg/view" dark />
 		</div>
 		<style>{`
